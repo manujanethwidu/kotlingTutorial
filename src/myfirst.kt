be9f557854ac98ca38bce8f72edeb332d9191afc
EDC <manujanethwidu@gmail.com>
2020-02-06 16:59:29 +0530

    8.2

--- src/myfirst.kt
+++ src/myfirst.kt
@@ -1,27 +1,40 @@
 fun main(args: Array<String>) {
  var dog = Dog()
-    dog.bread= "labrado"
-    dog.color = "Cream"
 
     dog.bark()
     dog.eat()
+
+    var cat = Cat()
+    cat.eat()
+
+    println(dog.color)
 }
 
 open class Animal() {
-    var color: String = ""
-    fun eat() {
+    open var color: String = "White"
+    open fun eat() {
         println("Animal eats")
     }
 }
 
 class Dog:Animal() {
+    override var color:String = "Black"
     var bread: String = ""
     fun bark() {
+    }
+
+   override fun eat() {
+        println("Dog eats")
     }
 }
 
 class Cat:Animal() {
     var age: Int = -1
     fun meow() {
+    }
+
+    override fun eat() {
+        super<Animal>.eat()
+        println("Cat eats")
     }
 }
