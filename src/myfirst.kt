57e6818c59e8063c0d6954229d16ab64db68225d
EDC <manujanethwidu@gmail.com>
2020-02-06 16:19:57 +0530

    8.2

--- src/myfirst.kt
+++ src/myfirst.kt
@@ -1,11 +1,18 @@
 fun main(args:Array<String>){
-    var student=Student("Sriyank")
+    var student=Student("Sriyank",10)
+    print(student.id)
 }
 
 class Student(_name:String){
     var name :String="dummy"
+    var id:Int= -1
     init {
         this.name=_name
         println("Name is ${name}")
+    }
+
+    constructor(name:String,id:Int):this(name){
+        this.id=id
+
     }
 }
